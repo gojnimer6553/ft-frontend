39c4c8e07f30c4c617d28d23bdb096c0990aee3d
gojnimer <gojnimer@gmail.com>
2025-08-12 18:32:57 +0000

    log

--- src/main.tsx
+++ src/main.tsx
@@ -24,7 +24,6 @@ const tolgee = Tolgee()
     apiKey: import.meta.env.VITE_APP_TOLGEE_API_KEY,
   });
 
-tolgee.run();
 // Create a new router instance
 const router = createRouter({
   routeTree,
@@ -61,4 +60,7 @@ if (rootElement && !rootElement.innerHTML) {
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
+reportWebVitals((...props) => {
+  console.log(...props);
+  console.log(tolgee);
+});
