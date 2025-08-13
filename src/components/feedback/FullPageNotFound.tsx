eb2c9cf3f4b9a8531667d673742498cddbbc68fd
gojnimer <gojnimer@gmail.com>
2025-08-13 16:16:50 +0000

    feat: implement FullPageLoader and FullPageNotFound components with translation support

--- src/components/feedback/FullPageNotFound.tsx
+++ src/components/feedback/FullPageNotFound.tsx
@@ -1,16 +1,14 @@
 import { useTranslate } from "@tolgee/react";
 
-export default function FullPageLoader() {
+export default function FullPageNotFound() {
   const { t } = useTranslate();
   return (
     <div className="flex items-center flex-col justify-center w-dvw h-dvh px-6 gap-4">
       <img
-        className="size-96 object-scale-down rounded-full animate-pulse"
+        className="size-96 object-scale-down rounded-full "
         src="/assets/main-logo.png"
       />
-      <p className="font-mono text-2xl animate-pulse">
-        {t("feedback.loading")}
-      </p>
+      <p className="font-mono text-2xl">{t("feedback.pageNotFound")}</p>
     </div>
   );
 }
