3835073d63ecd3752decce67cd169e45a6beaafa
gojnimer6553 <136547606+gojnimer6553@users.noreply.github.com>
2025-08-16 19:55:46 -0300

    feat: adopt shadcn style collapsible sidebar

---
+++ src/hooks/use-store.ts
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+/**
+ * This hook fixes hydration when persisting store data to localStorage.
+ */
+export const useStore = <T, F>(
+  store: (callback: (state: T) => unknown) => unknown,
+  callback: (state: T) => F
+) => {
+  const result = store(callback) as F;
+  const [data, setData] = useState<F>();
+
+  useEffect(() => {
+    setData(result);
+  }, [result]);
+
+  return data;
+};
