05d8abd3ea6526efe96067a7a2a8335e214a39ac
gojnimer6553 <136547606+gojnimer6553@users.noreply.github.com>
2025-08-16 17:39:16 -0300

    refactor: move chat scrollbar to route

--- src/components/chat/index.tsx
+++ src/components/chat/index.tsx
@@ -26,7 +26,7 @@ export function Chat() {
   ];
 
   return (
-    <div className="flex flex-1 min-h-0 flex-col items-center px-4 overflow-x-hidden overflow-y-auto">
+    <>
       <ChatHeader />
       <div className="flex w-full max-w-2xl flex-1 min-h-0 flex-col overflow-x-hidden overflow-y-auto">
         <ChatMessages messages={messages} isLoading={status !== "ready"} />
@@ -36,7 +36,7 @@ export function Chat() {
           placeholders={placeholders}
         />
       </div>
-    </div>
+    </>
   );
 }
 
